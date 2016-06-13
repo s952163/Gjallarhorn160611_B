c0862713443e2d417c3793e3b6f8b0b001762d14
s952163 <s995110@yahoo.co.jp>
2016-06-13 09:15:39 +0900

    Add project files.

---
+++ Gjallarhorn160611/Context3.fs
@@ -0,0 +1,51 @@
+﻿module Context3
+
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+open OxyPlot
+
+
+let create() =
+    let rnd = System.Random()   
+    let makeSeries n =
+        seq {for _ in 1..n -> rnd.NextDouble() - 0.5} |> Seq.scan (+) 0. |> Seq.mapi (fun x y -> DataPoint(float x,y)) |> Seq.toList
+        
+    let subject = Binding.createSubject () 
+    let date1 = Mutable.create <| System.DateTime.Now.AddYears(-1)
+    let date2 = Mutable.create <| System.DateTime.Now
+    
+    let daysDiff =  Signal.map2 (-) date2 date1
+    let chartData = daysDiff |> Signal.map (fun x -> makeSeries x.Days)
+    //let chartData = Mutable.create <| makeSeries daysDiff.Value.Days
+
+    date1 |> Binding.editDirect subject "startDt" noValidation
+    date2 |> Binding.editDirect subject "endDt" noValidation
+    //chartData |> Binding.editDirect subject "Data" noValidation
+
+    subject.Watch "daysDiff" (daysDiff |> Signal.map (fun x -> x.Days))
+    subject.Watch "Data" chartData
+
+    let _sub1 = date1 |> Signal.Subscription.create(fun n1 -> printfn "Start Date: %A" n1)  
+    let _sub2 = date2 |> Signal.Subscription.create(fun n1 -> printfn "End Date: %A" n1) 
+    
+    //let _sub3 = daysDiff |> Signal.Subscription.create(fun x -> chartData.Value <- makeSeries x.Days )        
+    
+    (*
+    let click _ =   out1.Value <- rnd.Next in1.Value  
+    
+    let clickcommand = subject.Command "Click" 
+
+    clickcommand
+    |> Observable.subscribe click
+    |> subject.AddDisposable
+     *)
+    
+    _sub2 |> subject.AddDisposable
+    _sub1 |> subject.AddDisposable
+    //_sub3 |> subject.AddDisposable
+
+    subject
+
+
+
---
+++ Gjallarhorn160611/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+open FsXaml
+open System.Windows
+
+//type MainWin = XAML<"MainWindow.xaml">
+type MainWin = XAML<"MainWindow2.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =     
+
+    let rnd = System.Random()
+    let num1 = Mutable.create 0
+    let num2 = Mutable.create 1
+    let click() = num1.Value <- rnd.Next(num2.Value)
+    let _sub1 = num2 |> Signal.Subscription.create (fun _ -> click())
+    let _sub2 = num1 |> Signal.Subscription.create(fun n1 -> printfn "The lucky number is: %d" n1)
+
+
+    num2.Value <- 30
+        
+    //printfn "%A" num1.Value
+
+
+    Gjallarhorn.Wpf.install true |> ignore
+
+    let app = Application()
+    //let win = MainWin(DataContext = Context.create())
+    //let win = MainWin(DataContext = Context2.create())
+    let win = MainWin(DataContext = Context3.create())
+    app.Run win
+    
